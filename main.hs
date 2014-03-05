929bffb2314aa401109d21722fb5d7e6d83b013c
Hai Vo <hai@hai-SVE15128CXS.(none)>
2014-03-05 15:24:28 -0800

    some changes

--- main.hs
+++ main.hs
@@ -1,28 +1,101 @@
 import System.IO  
 import Control.Monad
 
+class Troop a where
+    strength :: a -> Int 
 
-data Troop = Sworder | Arrower deriving (Eq, Show, Read)
+data Sworder = Sworder Int
+instance Troop Sworder where 
+    strength (Sworder x) = x
 
-data Sworder = SworderCommander (Int, Int) | SworderTroop String deriving (Eq, Show, Read)
+data Arrower = Arrower Int
+instance Troop Arrower where 
+    strength (Arrower x) = x
 
-data Arrower = ArrowerCommander (Int, Int) | ArrowerTroop Int  deriving (Eq, Show, Read)
+class Commander c where
+    command :: c -> Int
+    strCommander :: c -> Int
+    
 
-data Squad = SworderSquad [Sworder] | ArrowerSquad [Arrower] | ScatterSquad [Sworder] deriving (Eq, Show, Read)
+data SworderCommander = SworderCommander (Int,Sworder) 
+instance Commander SworderCommander where 
+    command (SworderCommander x) = (fst x)
+    strCommander (SworderCommander x) = strength (snd x)
+
+data ArrowerCommander = ArrowerCommander (Int,Arrower) 
+instance Commander ArrowerCommander where 
+    command (ArrowerCommander x) = (fst x)
+    strCommander (ArrowerCommander x) = strength (snd x)
 
 
+class Squad c where 
+    commander :: c -> Commander
+    iter :: c -> [Troop]
+
+data SworderSquad = SworderSquad SworderCommander [Sworder]
+instance Squad SworderSquad where
+    commander (SworderSquad x _) = x
+    iter (SworderSquad x y) = y ++ x 
+
+data ArrowerSquad = ArrowerSquad ArrowerCommander [Arrower]
+instance Squad ArrowerSquad where
+    commander (ArrowerSquad x _) = x
+    iter (ArrowerSquad x y) = y ++ x
+
+data ScatterSquad = ScatterSquad [Sworder]
+instance Squad ScatterSquad where
+    commander (ScatterSquad x _) = (SworderCommander (4, (Sworder 0)))
+    iter (ScatterSquad x) = x
+
+class Engagement x where 
+    add :: Troop -> x
 
 
+class EngageableGroup x where
+    engageSwordVSword :: x -> x -> [Engagement]
+    engageSwordVScatter :: x -> x -> [Engagement]
+    engageScatterVSword :: x -> x -> [Engagement]
+    engage :: (f -> b) -> [Engagement]
 
 
-main = do  
-        handle <- openFile "test.txt" ReadMode
-        mainloop handle sworder arrower scatterSworder
-        hClose handle
-        where 
-            sworder = []
-            arrower = []
-            scatterSworder = []
+data SworderVSSworder = SworderVSSworder SworderSquad SworderSquad
+instance EngageableGroup SworderVSSworder where
+    engageSwordVSword (SworderSquad x) (SworderSquad y) = if (length x > length y)
+                                                    then zip xs y
+                                                         engageSwordVScatter zs
+                                                         where
+                                                            (xs, zs) = splitAt (length y) x 
+                                                    else zip x ys 
+                                                         engageScatterVSword as 
+                                                         where
+                                                            (ys, as) = splitAt (length x) y
+
+    engageSwordVScatter (SworderSquad x) (ScatterSquad y) = if (length x > length y)
+                                                    then zip xs y
+                                                         engageSwordVScatter zs
+                                                         where
+                                                            (xs, zs) = splitAt (length y) x 
+                                                    else zip x ys 
+                                                         where
+                                                            (ys, as) = splitAt (length x) y
+    engageScatterVSword (ScatterSquad x) (SworderSquad y) = if (length x > length y)
+                                                    then zip xs y
+                                                         where
+                                                            (xs, zs) = splitAt (length y) x 
+                                                    else zip x ys 
+                                                         engageScatterVSword as
+                                                         where
+                                                            (ys, as) = splitAt (length x) y
+    engage (engageSwordVSword (SworderSquad x) (SworderSquad y)) = 
+
+
+main = print "test"
+        --let sworder = SworderTroop 5
+        --print (strength sworder)
+        --handle <- openFile "test.txt" ReadMode
+        --mainloop handle sworder arrower scatterSworder
+        --hClose handle
+        
 
 mainloop :: Handle -> [String] -> [String] -> [String] -> IO () 
 mainloop inh sworder arrower scatterSworder = 
